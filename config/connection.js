be178c50a907f4a23f4e08da7da1ee07a109919d
Biswajitbala88 <biswajitbala88@gmail.com>
2024-01-01 23:31:27 +0530

    modified connection file

--- config/connection.js
+++ config/connection.js
@@ -1,28 +1,29 @@
-const { MongoClient } = require('mongodb');
+const mongoose = require('mongoose');
 
 // Connection URL and Database Name
-const url = 'mongodb://localhost:27017';
-const dbName = 'mern';
+// const url = 'mongodb://localhost:27017';
+// const dbName = 'mern';
 
 // Create a new MongoClient
-const client = new MongoClient(url, { useNewUrlParser: true, useUnifiedTopology: true });
+// const client = new MongoClient(url, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect('mongodb://localhost:27017/mern');
 
-// Connect to the server
-async function connectToMongoDB() {
-    try {
-        await client.connect();
-        console.log('Connected successfully to server');
+// // Connect to the server
+// async function connectToMongoDB() {
+//     try {
+//         await client.connect();
+//         console.log('Connected successfully to server');
 
-        const database = client.db(dbName);
+//         const database = client.db(dbName);
 
-        // Perform operations using the database object
-    } catch (err) {
-        console.error('Error occurred while connecting to MongoDB:', err);
-    } finally {
-        // Close the connection
-        await client.close();
-        console.log('Connection to MongoDB closed');
-    }
-}
-// Call the function to connect
-connectToMongoDB();
+//         // Perform operations using the database object
+//     } catch (err) {
+//         console.error('Error occurred while connecting to MongoDB:', err);
+//     } finally {
+//         // Close the connection
+//         await client.close();
+//         console.log('Connection to MongoDB closed');
+//     }
+// }
+// // Call the function to connect
+// connectToMongoDB();
