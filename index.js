99d54629a20cc8096094d32c826d5af800c65c44
Biswajitbala88 <biswajitbala88@gmail.com>
2024-01-05 21:37:26 +0530

    improved DB connection

--- index.js
+++ index.js
@@ -1,41 +1,24 @@
-const port = 1200;
-
 // DB connection
-const mongoDbUrl = "mongodb://localhost:27017/mern";
+require("dotenv").config();
+const mongoDbUrl = process.env.MONGO_URL;
 const connectDB = require("./config/connection");
 connectDB(mongoDbUrl);
+const port = process.env.PORT || 1500;
 
 const express = require('express');
-
 const UserRoute = require('./routes/UserRoute');
 const ProductRoute = require('./routes/ProductRoute');
 const CategoryRoute = require('./routes/CategoryRoute');
 
-
-
 const app = express();
 const cors = require('cors');
-
 app.use(cors())
 app.use(express.json());
-
-
-
-
-
-
 
 ////////////// All routes /////////////////
 app.use('/', UserRoute);
 app.use('/product/', ProductRoute);
 app.use('/category/', CategoryRoute);
-
-
-
-
-
-
-
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
