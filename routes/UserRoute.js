beaeaa7d634abdf06276710721bb76e5503fc613
Biswajitbala88 <biswajitbala88@gmail.com>
2024-01-08 23:55:37 +0530

    modified user route

--- routes/UserRoute.js
+++ routes/UserRoute.js
@@ -1,17 +1,23 @@
 const express = require('express');
 const router = express.Router();
-const { signup, signin, updateUser, getAllUser } = require('../controllers/UserController');
+const { signup, signin, updateUser, getAllUser, userStats } = require('../controllers/UserController');
 
 
 
 // signup
 router.post("/api/signup", signup);
+
 // signin
 router.post("/api/signin", signin);
 
+// update user
 router.put("/api/update-user/:id", updateUser);
 
+// get all user
 router.get("/api/user", getAllUser);
+
+// user stats
+router.get("/api/user-stats", userStats);
 
 
 module.exports = router;
