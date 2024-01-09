52d2f1844aa3e2da2353a46c6eeee8a1cc5fce1e
Biswajitbala88 <biswajitbala88@gmail.com>
2024-01-09 23:49:08 +0530

    added product stats route

--- routes/ProductRoute.js
+++ routes/ProductRoute.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { addProduct, getAllProducts, getProductDetails, deleteProduct, updareProduct } = require('../controllers/ProductController');
+const { addProduct, getAllProducts, getProductDetails, deleteProduct, updareProduct, productStats } = require('../controllers/ProductController');
 
 
 
@@ -10,11 +10,15 @@ router.post("/", addProduct);
 // all-product
 router.get("/", getAllProducts);
 // product-details
-router.get("/:id", getProductDetails);
+router.get("/details/:id", getProductDetails);
 // product-details
 router.delete("/:id", deleteProduct);
 // update-product
 router.put("/:id", updareProduct);
+// user stats
+router.get("/product-stats", productStats);
+
+
 
 
 module.exports = router;
