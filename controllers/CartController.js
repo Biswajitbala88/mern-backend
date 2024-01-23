937a94231e3de30818e842a2d659e0ea798034c2
Biswajitbala88 <biswajitbala88@gmail.com>
2024-01-24 01:27:24 +0530

    added user-wise cart and update cart

--- controllers/CartController.js
+++ controllers/CartController.js
@@ -5,43 +5,81 @@ const CartModel = require('../models/CartModel');
 // create cart
 
 const createCart = async (req, resp)=>{
-    try{
-        const product_id = req.params.id;
-        const { user_id, quantity } = req.body;
-        let cart = await CartModel.findOne({ user_id });
-        if(cart){
-          const existingItem = cart.items.find(item => item.product_id === product_id);
-          if (existingItem) {
-              existingItem.quantity = quantity || 0;
-          } else {
-              // If the product is not in the cart, add it
-              cart.items.push({ product_id, quantity });
-          }
-          const saveCart = await cart.save();
-          resp.status(200).json({ message: "Cart created successfully", cart: saveCart });
-        }else{
-          const newCart = new CartModel({
-            user_id, items: [{ product_id, quantity }]
-          });
-          const saveCart = await newCart.save();
-          resp.status(200).json({ message: "Cart updated successfully", cart: saveCart });
-        }
-    } catch (error) {
-      resp.status(500).json({ message: "Getting error while create cart", error: message.error });
-    }
-  }
-
-  // get all cart
-const getAllCart = async (req, resp)=>{
   try{
-    const carts = await CartModel.find({});
-    if(carts.length>0){
-      resp.status(200).json({ message: "Cart found successsfully", cart: carts });
-    }else{
-      resp.status(400).json({ message: "No cart found" });
-    }
+      const product_id = req.params.id;
+      const { user_id, quantity } = req.body;
+      let cart = await CartModel.findOne({ user_id });
+      if(cart){
+        const existingItem = cart.items.find(item => item.product_id === product_id);
+        if (existingItem) {
+            existingItem.quantity = quantity || 0;
+        } else {
+            // If the product is not in the cart, add it
+            cart.items.push({ product_id, quantity });
+        }
+        const saveCart = await cart.save();
+        resp.status(200).json({ message: "Cart created successfully", cart: saveCart });
+      }else{
+        const newCart = new CartModel({
+          user_id, items: [{ product_id, quantity }]
+        });
+        const saveCart = await newCart.save();
+        resp.status(200).json({ message: "Cart updated successfully", cart: saveCart });
+      }
   } catch (error) {
-    resp.status(500).json({ message: "Error getting all cart", error: message.error });
+    resp.status(500).json({ message: "Getting error while create cart", error: message.error });
+  }
+}
+
+
+// update cart
+const updateCart = async (req, resp) => {
+  try {
+    const { user_id, product_id, quantity } = req.body;
+    // Find the cart based on user_id
+    let cart = await CartModel.findOne({ user_id });
+    // If the cart does not exist, create a new one
+    if (!cart) {
+      cart = new CartModel({ user_id, items: [] });
+    }
+    // Find the index of the product in the cart
+    const productIndex = cart.items.findIndex(item => item.product_id === product_id);
+    cart.items[productIndex].quantity = quantity;
+    // Save the updated cart
+    await cart.save();
+
+    resp.status(200).json({ message: "Cart update successful", cart: cart });
+  } catch (error) {
+    resp.status(500).json({ message: "Error updating cart", error: error });
+  }
+};
+
+
+
+
+// get all cart
+const getAllCart = async (req, resp)=>{
+try{
+  const carts = await CartModel.find({});
+  if(carts.length>0){
+    resp.status(200).json({ message: "Cart found successsfully", cart: carts });
+  }else{
+    resp.status(400).json({ message: "No cart found" });
+  }
+} catch (error) {
+  resp.status(500).json({ message: "Error getting all cart", error: message.error });
+}
+}
+
+// get user wise cart
+const getUserCart = async (req, resp)=>{
+try{
+    const user_id = req.body.user_id;
+    const cart = await CartModel.findOne({user_id});
+    // const productIds = cart.items.map(item => item.product_id);
+    resp.status(200).json({ message: "user cart found", cart: cart });
+  } catch (error) {
+    resp.status(500).json({ message: "error getting user cart", error: error });
   }
 }
 
@@ -76,4 +114,4 @@ const cartStats = async (req, resp) => {
 }
 
 
-module.exports = { createCart, getAllCart, cartStats };
+module.exports = { createCart, getAllCart, cartStats, getUserCart, updateCart };
--- routes/CartRoute.js
+++ routes/CartRoute.js
@@ -1,14 +1,20 @@
 const express = require('express');
 const router = express.Router();
-const { createCart, getAllCart, cartStats } = require('../controllers/CartController');
+const { createCart, getAllCart, cartStats, getUserCart, updateCart } = require('../controllers/CartController');
 
 
 
 
 // all-category
-router.get("/", getAllCart);
+router.get("/all-cart", getAllCart);
 // add-category
-router.post("/:id", createCart);
+router.post("/create-cart/:id", createCart);
+// user-wise cart
+router.post("/user-cart", getUserCart);
+// update cart
+router.post("/update-cart", updateCart);
+
+
 
 // cart stats
 router.get("/cart-stats", cartStats);
